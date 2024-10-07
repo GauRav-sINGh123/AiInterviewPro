ba086195a78629f5dde2ef391447841ee7e1f720
Gaurav Singh <gksingh947@gmail.com>
2024-10-07 22:03:53 +0530

    React Speach Integrate

--- src/app/dashboard/interview/[interviewId]/getStarted/_components/Record.tsx
+++ src/app/dashboard/interview/[interviewId]/getStarted/_components/Record.tsx
@@ -1,11 +1,57 @@
-import Image from 'next/image'
-
+'use client'
+import { useState } from "react";
+import Image from "next/image";
+import Webcam from "react-webcam";
+import useSpeechToText from 'react-hook-speech-to-text';
 function Record() {
+  const [isCameraOpen, setIsCameraOpen] = useState<boolean>(false);
+  const {
+    error,
+    interimResult,
+    isRecording,
+    results,
+    startSpeechToText,
+    stopSpeechToText,
+  } = useSpeechToText({
+    continuous: true,
+    useLegacyResults: false
+  });
+
+  if (error) return <p>Web Speech API is not available in this browser 🤷‍</p>;
   return (
-    <div className='p-5 border rounded-lg mt-16 bg-black h-[400px] flex flex-col justify-center items-center'>
-     <Image src="/Camera.png" alt="record" width={200} height={200}/> 
+    <div>
+      <div className="p-5 border rounded-lg mt-20 bg-black h-[350px] flex flex-col justify-center items-center">
+     
+       {
+        isCameraOpen?(
+          <Webcam mirrored={true} style={{ width: 500, height: 300 }} />
+        ):
+        (
+          <Image src={"/Camera.png"} alt="logo" width={180} height={180}/>
+        )
+       }
+      </div>
+      <div className="flex justify-center">
+        <button 
+        onClick={() => setIsCameraOpen(!isCameraOpen)}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 mt-2 rounded-lg cursor-pointer">{isCameraOpen ? 'Close Camera' : 'Record'}</button>
+           
+      </div>
+      <div className="flex justify-center gap-3">
+      <h1>Recording: {isRecording.toString()}</h1>
+      <button onClick={isRecording ? stopSpeechToText : startSpeechToText}>
+        {isRecording ? 'Stop Recording' : 'Start Recording'}
+      </button>
+      <ul>
+        {results.map((result:any) => (
+          <li key={result.timestamp}>{result.transcript}</li>
+        ))}
+        {interimResult && <li>{interimResult}</li>}
+      </ul>
     </div>
-  )
+        
+    </div>
+  );
 }
 
-export default Record
+export default Record;
